0b6558e1bb27dfffaf348ae62a2a2bd0e9b5256c
mbrad26 <mbrad26@gmail.com>
2020-04-28 20:15:41 +0100

    post redirect get pattern

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1_name]
-    @player_2 = params[:player_2_name]
+    session[:player_1] = params[:player_1_name]
+    session[:player_2] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
   # start the server if ruby file executed directly
