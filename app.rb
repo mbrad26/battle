b121419487425875843be3eec8cc9bc7651303f9
mbrad26 <mbrad26@gmail.com>
2020-04-28 19:48:16 +0100

    entering players

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get '/' do
-    'Testing infrastructure working'
+    erb :index
   end
 
+  post '/names' do
+    @player_1 = params[:player_1_name]
+    @player_2 = params[:player_2_name]
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
