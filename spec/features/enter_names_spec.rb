21943c5808b494e02600b03ea8267054cc7aa8f5
mbrad26 <mbrad26@gmail.com>
2020-04-29 07:52:49 +0100

    viewing hit points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -8,3 +8,14 @@ feature 'Enter names' do
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
+
+feature 'View hit points' do
+  scenario 'see Player  2 hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+
+    expect(page).to have_content 'Mittens: 60HP'
+  end
+end
